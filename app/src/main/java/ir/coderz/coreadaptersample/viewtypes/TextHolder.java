35c9d43d1c3443232989f01b308bba6e9bcc78c5
mahdi delavar <md_delavar74@yahoo.com>
2018-07-04 18:36:03 +0430

    add binding

--- app/src/main/java/ir/coderz/coreadaptersample/viewtypes/TextHolder.java
+++ app/src/main/java/ir/coderz/coreadaptersample/viewtypes/TextHolder.java
@@ -1,24 +1,28 @@
 package ir.coderz.coreadaptersample.viewtypes;
 
+import android.databinding.ViewDataBinding;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
 
 import ir.coderz.coreadaptersample.R;
+import ir.coderz.coreadaptersample.databinding.TextItemBinding;
 
 /**
  * Created by sajad on 6/30/16.
  */
 public class TextHolder extends RecyclerView.ViewHolder {
 
-    private final TextView textView;
+    TextItemBinding binding;
+//    private final TextView textView;
 
-    public TextHolder(View itemView) {
-        super(itemView);
-        textView = (TextView) itemView.findViewById(R.id.text);
+    public TextHolder(ViewDataBinding binding) {
+        super(binding.getRoot());
+        this.binding = (TextItemBinding) binding;
+//        textView = (TextView) itemView.findViewById(R.id.text);
     }
 
-    public TextView getTextView() {
-        return textView;
-    }
+//    public TextView getTextView() {
+//        return textView;
+//    }
 }
